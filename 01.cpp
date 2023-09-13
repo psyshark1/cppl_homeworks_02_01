14fac8c07baa9a9c8f3aeb543bb76845b6e1bf03
Andrey <andrey.zalozhenkov@pikta.ru>
2023-09-13 17:06:12 +0400

    Добавьте файлы проекта.

---
+++ 01.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct point
+{
+	double m_x;
+	double m_y;
+	point(double x, double y)
+	{
+		m_x = x;
+		m_y = y;
+	}
+};
+
+void print_point(const point& point_object);
+
+int main()
+{
+	int i{0};
+	for (; i < 5; ++i)
+	{
+		point my_point(i, 2.0 * i);
+		print_point(my_point);
+	}
+	return 0;
+}
+
+void print_point(const point& point_object) {
+	std::cout << "x:" << point_object.m_x << ", y: "
+		<< point_object.m_y << std::endl;
+}
